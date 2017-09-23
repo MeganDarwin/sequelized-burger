6e01884a8e3f03971b49195e8e4fa3e447c055ee
MeganDarwin <MeganDarwin@gmail.com>
2017-09-22 22:06:27 -0400

    readme

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var db = require("./models");
+var db = require("/models");
 
 var PORT = process.env.PORT || 3000
 
